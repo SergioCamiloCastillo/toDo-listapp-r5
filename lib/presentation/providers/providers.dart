fb061334640bf76e2cb56753554924b03716dec3
SergioCamiloCastillo <scastillonu@uniminuto.edu.co>
2024-01-22 12:39:23 -0500

    delete unnecessary thing for the app

--- lib/presentation/providers/providers.dart
+++ lib/presentation/providers/providers.dart
@@ -1,3 +1,2 @@
 export 'package:todo_listapp_r5/presentation/providers/tasks_provider.dart';
 export 'package:todo_listapp_r5/presentation/providers/tasks_repository_provider.dart';
-export 'package:todo_listapp_r5/presentation/providers/forms/task_form_provider.dart';
--- lib/shared/infrastructure/inputs/inputs.dart
+++
@@ -1,3 +0,0 @@
-export 'package:todo_listapp_r5/shared/infrastructure/inputs/description.dart';
-export 'package:todo_listapp_r5/shared/infrastructure/inputs/title.dart';
-export 'package:todo_listapp_r5/shared/infrastructure/inputs/date.dart';
